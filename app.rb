8fb5b8b9bbfecfacddda96a48e30e61a2608bba1
ollieh-m <ollie.hm@cantab.net>
2016-04-25 17:47:05 +0100

    Add pictures with borders to /cat

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "<h3>Hello</h3>"
 end
@@ -7,3 +9,20 @@ end
 get '/secret' do
 	"<h1>Omar plays accordion!!</h1>"
 end
+
+get '/colours' do
+	"<h2>I am red</h2>"
+end
+
+get '/the_one_and_only' do
+	"1"
+end
+
+get '/cat' do
+	'<style>
+ 		img {border-style: dashed; border-color: red;}
+	</style>
+	<img src="http://bit.ly/1eze8aE">
+	<img src="http://bit.ly/1eze8aE">'
+end
+

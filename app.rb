aac77c896c4409f6a28b64ee26aabdff77a1dfec
Omar Alvarez <omajul2@hotmail.com>
2016-04-26 07:40:44 +0000

    Step 9 - Sinatra: Introducing views

--- app.rb
+++ app.rb
@@ -3,26 +3,10 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  "<h3>Hello</h3>"
-end
-
-get '/secret' do
-	"<h1>Omar plays accordion!!</h1>"
-end
-
-get '/colours' do
-	"<h2>I am red</h2>"
-end
-
-get '/the_one_and_only' do
-	"1"
+  erb(:index)
 end
 
 get '/cat' do
-	'<style>
- 		img {border-style: dashed; border-color: red;}
-	</style>
-	<img src="http://bit.ly/1eze8aE">
-	<img src="http://bit.ly/1eze8aE">'
+	erb(:cat)
 end
 

2888f8c89f90745229901577e1d8a07837148234
Omar Alvarez <omajul2@hotmail.com>
2016-04-26 10:26:57 +0000

    Step 12 - Sinatra: Params

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
+	'Hello'
+end
+
+get '/random-cat' do
 	@name = ['Amigo', 'Oscar', 'Viking'].sample
+	@age = rand(1..5)
+  erb(:index)
+end
+
+get '/named-cat' do
+	p params
+	@name = params[:name]
 	@age = rand(1..5)
   erb(:index)
 end
